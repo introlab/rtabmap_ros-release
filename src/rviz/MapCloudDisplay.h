4c02edec7df32742bdf61a5cb074d7d96eca6d2b
matlabbe <matlabbe@gmail.com>
2016-05-06 13:20:40 -0400

    Imported upstream version '0.11.4' of 'upstream'

--- src/rviz/MapCloudDisplay.h
+++ src/rviz/MapCloudDisplay.h
@@ -106,8 +106,10 @@ public:
 	rviz::EnumProperty* style_property_;
 	rviz::IntProperty* cloud_decimation_;
 	rviz::FloatProperty* cloud_max_depth_;
+	rviz::FloatProperty* cloud_min_depth_;
 	rviz::FloatProperty* cloud_voxel_size_;
 	rviz::FloatProperty* cloud_filter_floor_height_;
+	rviz::FloatProperty* cloud_filter_ceiling_height_;
 	rviz::FloatProperty* node_filtering_radius_;
 	rviz::FloatProperty* node_filtering_angle_;
 	rviz::BoolProperty* download_map_;
