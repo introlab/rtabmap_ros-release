eb68a08b385a76c35dc06f2be17aa5bafc5a9e01
matlabbe <matlabbe@gmail.com>
2023-04-09 17:39:36 -0700

    Imported upstream version '0.21.1' of 'upstream'

--- rtabmap_util/src/nodelets/lidar_deskewing.cpp
+++ rtabmap_util/src/nodelets/lidar_deskewing.cpp
@@ -1,106 +1,121 @@
 
-#include <rtabmap_util/lidar_deskewing.hpp>
+#include <ros/ros.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <nodelet/nodelet.h>
 
-#include <laser_geometry/laser_geometry.hpp>
+#include <tf/transform_listener.h>
 
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include <laser_geometry/laser_geometry.h>
+
+#include <pcl_ros/transforms.h>
+
+#include <rtabmap/core/util3d_transforms.h>
+#include <rtabmap/utilite/UTimer.h>
 #include <rtabmap_conversions/MsgConversion.h>
 
 namespace rtabmap_util
 {
 
-LidarDeskewing::LidarDeskewing(const rclcpp::NodeOptions & options) :
-	Node("lidar_deskewing", options),
-	waitForTransformDuration_(0.01),
-	slerp_(false)
+class LidarDeskewing : public nodelet::Nodelet
 {
-	tfBuffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-	//auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
-	//	this->get_node_base_interface(),
-	//	this->get_node_timers_interface());
-	//tfBuffer_->setCreateTimerInterface(timer_interface);
-	tfListener_ = std::make_shared<tf2_ros::TransformListener>(*tfBuffer_);
-
-	int queueSize = 5;
-	int qos = 0;
-	queueSize = this->declare_parameter("queue_size", queueSize);
-	qos = this->declare_parameter("qos", qos);
-	fixedFrameId_ = this->declare_parameter("fixed_frame_id", fixedFrameId_);
-	waitForTransformDuration_ = this->declare_parameter("wait_for_transform", waitForTransformDuration_);
-	slerp_ = this->declare_parameter("slerp", slerp_);
-
-	RCLCPP_INFO(this->get_logger(), "  fixed_frame_id:  %s", fixedFrameId_.c_str());
-	RCLCPP_INFO(this->get_logger(), "  wait_for_transform:  %fs", waitForTransformDuration_);
-	RCLCPP_INFO(this->get_logger(), "  slerp:  %s", slerp_?"true":"false");
-
-	if(fixedFrameId_.empty())
+public:
+	LidarDeskewing() :
+		waitForTransformDuration_(0.01),
+		slerp_(false),
+		tfListener_(0)
 	{
-		RCLCPP_FATAL(this->get_logger(), "fixed_frame_id parameter cannot be empty!");
 	}
 
-	subScan_ = create_subscription<sensor_msgs::msg::LaserScan>("input_scan", rclcpp::QoS(queueSize).reliability((rmw_qos_reliability_policy_t)qos), std::bind(&LidarDeskewing::callbackScan, this, std::placeholders::_1));
-	subCloud_ = create_subscription<sensor_msgs::msg::PointCloud2>("input_cloud", rclcpp::QoS(queueSize).reliability((rmw_qos_reliability_policy_t)qos), std::bind(&LidarDeskewing::callbackCloud, this, std::placeholders::_1));
-
-	pubScan_ = create_publisher<sensor_msgs::msg::PointCloud2>(std::string(subScan_->get_topic_name()) + "/deskewed", rclcpp::QoS(1).reliability((rmw_qos_reliability_policy_t)qos));
-	pubCloud_ = create_publisher<sensor_msgs::msg::PointCloud2>(std::string(subCloud_->get_topic_name()) + "/deskewed", rclcpp::QoS(1).reliability((rmw_qos_reliability_policy_t)qos));
-}
-
-LidarDeskewing::~LidarDeskewing()
-{
-}
-
-void LidarDeskewing::callbackScan(const sensor_msgs::msg::LaserScan::ConstSharedPtr msg)
-{
-	// make sure the frame of the laser is updated during the whole scan time
-	rtabmap::Transform tmpT = rtabmap_conversions::getTransform(
-			msg->header.frame_id,
-			fixedFrameId_,
-			msg->header.stamp,
-			rclcpp::Time(msg->header.stamp.sec, msg->header.stamp.nanosec) + rclcpp::Duration::from_seconds(msg->ranges.size()*msg->time_increment),
-			*tfBuffer_,
-			waitForTransformDuration_);
-	if(tmpT.isNull())
+	virtual ~LidarDeskewing()
 	{
-		return;
 	}
 
-	sensor_msgs::msg::PointCloud2 scanOut;
-	laser_geometry::LaserProjection projection;
-	projection.transformLaserScanToPointCloud(fixedFrameId_, *msg, scanOut, *tfBuffer_);
-
-	rtabmap::Transform t = rtabmap_conversions::getTransform(msg->header.frame_id, scanOut.header.frame_id, msg->header.stamp, *tfBuffer_, waitForTransformDuration_);
-	if(t.isNull())
+private:
+	virtual void onInit()
 	{
-		RCLCPP_ERROR(this->get_logger(), "Cannot transform back projected scan from \"%s\" frame to \"%s\" frame at time %fs.",
-				scanOut.header.frame_id.c_str(), msg->header.frame_id.c_str(), rtabmap_conversions::timestampFromROS(msg->header.stamp));
-		return;
+		tfListener_ = new tf::TransformListener();
+		ros::NodeHandle & nh = getNodeHandle();
+		ros::NodeHandle & pnh = getPrivateNodeHandle();
+
+		pnh.param("fixed_frame_id", fixedFrameId_, fixedFrameId_);
+		pnh.param("wait_for_transform", waitForTransformDuration_, waitForTransformDuration_);
+		pnh.param("slerp", slerp_, slerp_);
+
+		NODELET_INFO("fixed_frame_id:  %s", fixedFrameId_.c_str());
+		NODELET_INFO("wait_for_transform:  %fs", waitForTransformDuration_);
+		NODELET_INFO("slerp:  %s", slerp_?"true":"false");
+
+		if(fixedFrameId_.empty())
+		{
+			NODELET_FATAL("fixed_frame_id parameter cannot be empty!");
+		}
+
+		pubScan_ = nh.advertise<sensor_msgs::PointCloud2>(nh.resolveName("input_scan") + "/deskewed", 1);
+		pubCloud_ = nh.advertise<sensor_msgs::PointCloud2>(nh.resolveName("input_cloud") + "/deskewed", 1);
+		subScan_ = nh.subscribe("input_scan", 1, &LidarDeskewing::callbackScan, this);
+		subCloud_ = nh.subscribe("input_cloud", 1, &LidarDeskewing::callbackCloud, this);
 	}
 
-	sensor_msgs::msg::PointCloud2 scanOutDeskewed;
-	rtabmap_conversions::transformPointCloud(t.toEigen4f(), scanOut, scanOutDeskewed);
-	pubScan_->publish(scanOutDeskewed);
-}
-
-void LidarDeskewing::callbackCloud(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg)
-{
-	sensor_msgs::msg::PointCloud2 msgDeskewed;
-	if(rtabmap_conversions::deskew(*msg, msgDeskewed, fixedFrameId_, *tfBuffer_, waitForTransformDuration_, slerp_))
+	void callbackScan(const sensor_msgs::LaserScanConstPtr & msg)
 	{
-		pubCloud_->publish(msgDeskewed);
+		// make sure the frame of the laser is updated during the whole scan time
+		rtabmap::Transform tmpT = rtabmap_conversions::getTransform(
+				msg->header.frame_id,
+				fixedFrameId_,
+				msg->header.stamp,
+				msg->header.stamp + ros::Duration().fromSec(msg->ranges.size()*msg->time_increment),
+				*tfListener_,
+				waitForTransformDuration_);
+		if(tmpT.isNull())
+		{
+			return;
+		}
+
+		sensor_msgs::PointCloud2 scanOut;
+		laser_geometry::LaserProjection projection;
+		projection.transformLaserScanToPointCloud(fixedFrameId_, *msg, scanOut, *tfListener_);
+
+		sensor_msgs::PointCloud2 scanOutDeskewed;
+		if(!pcl_ros::transformPointCloud(msg->header.frame_id, scanOut, scanOutDeskewed, *tfListener_))
+		{
+			ROS_ERROR("Cannot transform back projected scan from \"%s\" frame to \"%s\" frame at time %fs.",
+					fixedFrameId_.c_str(), msg->header.frame_id.c_str(), msg->header.stamp.toSec());
+			return;
+		}
+		pubScan_.publish(scanOutDeskewed);
 	}
-	else
+
+	void callbackCloud(const sensor_msgs::PointCloud2ConstPtr & msg)
 	{
-		// Just republish the msg to not breakdown downstream
-		// A warning should be already shown (see deskew() source code)
-		RCLCPP_WARN(this->get_logger(), "deskewing failed! returning possible skewed cloud!");
-		pubCloud_->publish(*msg);
+		sensor_msgs::PointCloud2 msgDeskewed;
+		if(rtabmap_conversions::deskew(*msg, msgDeskewed, fixedFrameId_, *tfListener_, waitForTransformDuration_, slerp_))
+		{
+			pubCloud_.publish(msgDeskewed);
+		}
+		else
+		{
+			// Just republish the msg to not breakdown downstream
+			// A warning should be already shown (see deskew() source code)
+			ROS_WARN("deskewing failed! returning possible skewed cloud!");
+			pubCloud_.publish(msg);
+		}
 	}
+
+private:
+	ros::Publisher pubScan_;
+	ros::Publisher pubCloud_;
+	ros::Subscriber subScan_;
+	ros::Subscriber subCloud_;
+	std::string fixedFrameId_;
+	double waitForTransformDuration_;
+	bool slerp_;
+	tf::TransformListener * tfListener_;
+};
+
+PLUGINLIB_EXPORT_CLASS(rtabmap_util::LidarDeskewing, nodelet::Nodelet);
 }
 
-}
 
-#include "rclcpp_components/register_node_macro.hpp"
-
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be discoverable when its library
-// is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(rtabmap_util::LidarDeskewing)
--- rtabmap_viz/include/rtabmap_viz/GuiWrapper.h
+++ rtabmap_viz/include/rtabmap_viz/GuiWrapper.h
@@ -28,24 +28,19 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef GUIWRAPPER_H_
 #define GUIWRAPPER_H_
 
-#include <rtabmap_viz/visibility.h>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp/executor.hpp>
-#include "rtabmap_msgs/msg/info.hpp"
-#include "rtabmap_msgs/msg/map_data.hpp"
-#include "rtabmap_msgs/msg/odom_info.hpp"
-#include "rtabmap_msgs/msg/goal.hpp"
+#include <ros/ros.h>
+#include "rtabmap_msgs/Info.h"
+#include "rtabmap_msgs/MapData.h"
+#include "rtabmap_msgs/OdomInfo.h"
+#include "rtabmap_msgs/Goal.h"
 #include "rtabmap/utilite/UEventsHandler.h"
 #include "rtabmap/core/Transform.h"
 
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
+#include <tf/transform_listener.h>
 
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <nav_msgs/msg/path.hpp>
-#include <std_msgs/msg/bool.hpp>
-#include <std_msgs/msg/int32_multi_array.hpp>
-#include "rtabmap_msgs/srv/get_map.hpp"
+#include <geometry_msgs/TwistStamped.h>
+#include <nav_msgs/Path.h>
+#include <std_msgs/Bool.h>
 
 #include <rtabmap_sync/CommonDataSubscriber.h>
 
@@ -59,67 +54,64 @@ class QApplication;
 
 namespace rtabmap_viz {
 
-class GuiWrapper : public rclcpp::Node, public UEventsHandler, public rtabmap_sync::CommonDataSubscriber
+class GuiWrapper : public UEventsHandler, public rtabmap_sync::CommonDataSubscriber
 {
 public:
-	RTABMAP_VIZ_PUBLIC
-	explicit GuiWrapper(const rclcpp::NodeOptions & options);
+	GuiWrapper(int & argc, char** argv);
 	virtual ~GuiWrapper();
 
 protected:
 	virtual bool handleEvent(UEvent * anEvent);
 
 private:
-	void infoMapCallback(const rtabmap_msgs::msg::Info::ConstSharedPtr infoMsg, const rtabmap_msgs::msg::MapData::ConstSharedPtr mapMsg);
-	void goalPathCallback(const rtabmap_msgs::msg::Goal::ConstSharedPtr goalMsg, const nav_msgs::msg::Path::ConstSharedPtr pathMsg);
-	void goalReachedCallback(const std_msgs::msg::Bool::ConstSharedPtr value);
+	void infoMapCallback(const rtabmap_msgs::InfoConstPtr & infoMsg, const rtabmap_msgs::MapDataConstPtr & mapMsg);
+	void goalPathCallback(const rtabmap_msgs::GoalConstPtr & goalMsg, const nav_msgs::PathConstPtr & pathMsg);
+	void goalReachedCallback(const std_msgs::BoolConstPtr & value);
 
 	virtual void commonMultiCameraCallback(
-			const nav_msgs::msg::Odometry::ConstSharedPtr & odomMsg,
-			const rtabmap_msgs::msg::UserData::ConstSharedPtr & userDataMsg,
+			const nav_msgs::OdometryConstPtr & odomMsg,
+			const rtabmap_msgs::UserDataConstPtr & userDataMsg,
 			const std::vector<cv_bridge::CvImageConstPtr> & imageMsgs,
 			const std::vector<cv_bridge::CvImageConstPtr> & depthMsgs,
-			const std::vector<sensor_msgs::msg::CameraInfo> & cameraInfoMsgs,
-			const std::vector<sensor_msgs::msg::CameraInfo> & depthCameraInfoMsgs,
-			const sensor_msgs::msg::LaserScan & scanMsg,
-			const sensor_msgs::msg::PointCloud2 & scan3dMsg,
-			const rtabmap_msgs::msg::OdomInfo::ConstSharedPtr& odomInfoMsg,
-			const std::vector<rtabmap_msgs::msg::GlobalDescriptor> & globalDescriptorMsgs = std::vector<rtabmap_msgs::msg::GlobalDescriptor>(),
-			const std::vector<std::vector<rtabmap_msgs::msg::KeyPoint> > & localKeyPoints = std::vector<std::vector<rtabmap_msgs::msg::KeyPoint> >(),
-			const std::vector<std::vector<rtabmap_msgs::msg::Point3f> > & localPoints3d = std::vector<std::vector<rtabmap_msgs::msg::Point3f> >(),
+			const std::vector<sensor_msgs::CameraInfo> & cameraInfoMsgs,
+			const std::vector<sensor_msgs::CameraInfo> & depthCameraInfoMsgs,
+			const sensor_msgs::LaserScan& scan2dMsg,
+			const sensor_msgs::PointCloud2& scan3dMsg,
+			const rtabmap_msgs::OdomInfoConstPtr& odomInfoMsg,
+			const std::vector<rtabmap_msgs::GlobalDescriptor> & globalDescriptorMsgs = std::vector<rtabmap_msgs::GlobalDescriptor>(),
+			const std::vector<std::vector<rtabmap_msgs::KeyPoint> > & localKeyPoints = std::vector<std::vector<rtabmap_msgs::KeyPoint> >(),
+			const std::vector<std::vector<rtabmap_msgs::Point3f> > & localPoints3d = std::vector<std::vector<rtabmap_msgs::Point3f> >(),
 			const std::vector<cv::Mat> & localDescriptors = std::vector<cv::Mat>());
 	virtual void commonStereoCallback(
-			const nav_msgs::msg::Odometry::ConstSharedPtr & odomMsg,
-			const rtabmap_msgs::msg::UserData::ConstSharedPtr & userDataMsg,
+			const nav_msgs::OdometryConstPtr & odomMsg,
+			const rtabmap_msgs::UserDataConstPtr & userDataMsg,
 			const cv_bridge::CvImageConstPtr& leftImageMsg,
 			const cv_bridge::CvImageConstPtr& rightImageMsg,
-			const sensor_msgs::msg::CameraInfo& leftCamInfoMsg,
-			const sensor_msgs::msg::CameraInfo& rightCamInfoMsg,
-			const sensor_msgs::msg::LaserScan & scan2dMsg,
-			const sensor_msgs::msg::PointCloud2 & scan3dMsg,
-			const rtabmap_msgs::msg::OdomInfo::ConstSharedPtr& odomInfoMsg,
-			const std::vector<rtabmap_msgs::msg::GlobalDescriptor> & globalDescriptorMsgs = std::vector<rtabmap_msgs::msg::GlobalDescriptor>(),
-			const std::vector<rtabmap_msgs::msg::KeyPoint> & localKeyPoints = std::vector<rtabmap_msgs::msg::KeyPoint>(),
-			const std::vector<rtabmap_msgs::msg::Point3f> & localPoints3d = std::vector<rtabmap_msgs::msg::Point3f>(),
+			const sensor_msgs::CameraInfo& leftCamInfoMsg,
+			const sensor_msgs::CameraInfo& rightCamInfoMsg,
+			const sensor_msgs::LaserScan& scan2dMsg,
+			const sensor_msgs::PointCloud2& scan3dMsg,
+			const rtabmap_msgs::OdomInfoConstPtr& odomInfoMsg,
+			const std::vector<rtabmap_msgs::GlobalDescriptor> & globalDescriptorMsgs = std::vector<rtabmap_msgs::GlobalDescriptor>(),
+			const std::vector<rtabmap_msgs::KeyPoint> & localKeyPoints = std::vector<rtabmap_msgs::KeyPoint>(),
+			const std::vector<rtabmap_msgs::Point3f> & localPoints3d = std::vector<rtabmap_msgs::Point3f>(),
 			const cv::Mat & localDescriptors = cv::Mat());
 	virtual void commonLaserScanCallback(
-			const nav_msgs::msg::Odometry::ConstSharedPtr & odomMsg,
-			const rtabmap_msgs::msg::UserData::ConstSharedPtr & userDataMsg,
-			const sensor_msgs::msg::LaserScan & scan2dMsg,
-			const sensor_msgs::msg::PointCloud2 & scan3dMsg,
-			const rtabmap_msgs::msg::OdomInfo::ConstSharedPtr& odomInfoMsg,
-			const rtabmap_msgs::msg::GlobalDescriptor & globalDescriptor = rtabmap_msgs::msg::GlobalDescriptor());
+			const nav_msgs::OdometryConstPtr & odomMsg,
+			const rtabmap_msgs::UserDataConstPtr & userDataMsg,
+			const sensor_msgs::LaserScan& scan2dMsg,
+			const sensor_msgs::PointCloud2& scan3dMsg,
+			const rtabmap_msgs::OdomInfoConstPtr& odomInfoMsg,
+			const rtabmap_msgs::GlobalDescriptor & globalDescriptor = rtabmap_msgs::GlobalDescriptor());
 
 	virtual void commonOdomCallback(
-			const nav_msgs::msg::Odometry::ConstSharedPtr & odomMsg,
-			const rtabmap_msgs::msg::UserData::ConstSharedPtr & userDataMsg,
-			const rtabmap_msgs::msg::OdomInfo::ConstSharedPtr& odomInfoMsg);
+			const nav_msgs::OdometryConstPtr & odomMsg,
+			const rtabmap_msgs::UserDataConstPtr & userDataMsg,
+			const rtabmap_msgs::OdomInfoConstPtr& odomInfoMsg);
 
-	void defaultCallback(const nav_msgs::msg::Odometry::SharedPtr & odomMsg);
+	void defaultCallback(const nav_msgs::OdometryConstPtr & odomMsg);
 
-	void processRequestedMap(const rtabmap_msgs::msg::MapData & map);
-	bool callEmptyService(const std::string & name);
-	bool callMapDataService(const std::string & name, bool global, bool optimized, bool graphOnly);
+	void processRequestedMap(const rtabmap_msgs::MapData & map);
 
 private:
 	rtabmap::PreferencesDialog * prefDialog_;
@@ -131,29 +123,29 @@ private:
 	// odometry subscription stuffs
 	std::string frameId_;
 	std::string odomFrameId_;
-	double waitForTransform_;
+	bool waitForTransform_;
+	double waitForTransformDuration_;
 	bool odomSensorSync_;
 	double maxOdomUpdateRate_;
-	std::shared_ptr<tf2_ros::Buffer> tfBuffer_;
-	std::shared_ptr<tf2_ros::TransformListener> tfListener_;
+	tf::TransformListener tfListener_;
 
-	rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr republishNodeDataPub_;
+	ros::Publisher republishNodeDataPub_;
 
-	message_filters::Subscriber<rtabmap_msgs::msg::Info> infoTopic_;
-	message_filters::Subscriber<rtabmap_msgs::msg::MapData> mapDataTopic_;
+	message_filters::Subscriber<rtabmap_msgs::Info> infoTopic_;
+	message_filters::Subscriber<rtabmap_msgs::MapData> mapDataTopic_;
 
-	message_filters::Subscriber<rtabmap_msgs::msg::Goal> goalTopic_;
-	message_filters::Subscriber<nav_msgs::msg::Path> pathTopic_;
-	rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr goalReachedTopic_;
+	message_filters::Subscriber<rtabmap_msgs::Goal> goalTopic_;
+	message_filters::Subscriber<nav_msgs::Path> pathTopic_;
+	ros::Subscriber goalReachedTopic_;
 
 	typedef message_filters::sync_policies::ExactTime<
-			rtabmap_msgs::msg::Info,
-			rtabmap_msgs::msg::MapData> MyInfoMapSyncPolicy;
+			rtabmap_msgs::Info,
+			rtabmap_msgs::MapData> MyInfoMapSyncPolicy;
 	message_filters::Synchronizer<MyInfoMapSyncPolicy> * infoMapSync_;
 
 	typedef message_filters::sync_policies::ExactTime<
-			rtabmap_msgs::msg::Goal,
-			nav_msgs::msg::Path> MyGoalPathSyncPolicy;
+			rtabmap_msgs::Goal,
+			nav_msgs::Path> MyGoalPathSyncPolicy;
 	message_filters::Synchronizer<MyGoalPathSyncPolicy> * goalPathSync_;
 };
 
