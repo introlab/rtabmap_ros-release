534eefe2fafbbc8cf2f9a0168887eb38fe0b9ef8
matlabbe <matlabbe@gmail.com>
2024-01-04 08:53:23 -0800

    Rebase from 'upstream'

--- rtabmap_sync/include/rtabmap_sync/rgbdx_sync.hpp
+++ rtabmap_sync/include/rtabmap_sync/rgbdx_sync.hpp
@@ -41,6 +41,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "rtabmap_msgs/msg/rgbd_image.hpp"
 #include "rtabmap_msgs/msg/rgbd_images.hpp"
 #include "rtabmap_sync/CommonDataSubscriber.h"
+#include "rtabmap_sync/SyncDiagnostic.h"
 
 namespace rtabmap_sync
 {
@@ -68,14 +69,11 @@ private:
 	DATA_SYNCS8(rgbd8, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage, rtabmap_msgs::msg::RGBDImage)
 
 private:
-	std::thread * warningThread_;
-	bool callbackCalled_;
-
-	std::string subscribedTopicsMsg_;
-
 	rclcpp::Publisher<rtabmap_msgs::msg::RGBDImages>::SharedPtr rgbdImagesPub_;
 
 	std::vector<message_filters::Subscriber<rtabmap_msgs::msg::RGBDImage>*> rgbdSubs_;
+
+	std::unique_ptr<SyncDiagnostic> syncDiagnostic_;
 };
 
 }
