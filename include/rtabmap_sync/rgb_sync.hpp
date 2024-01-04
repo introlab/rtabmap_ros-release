9c9a72d4dca1e82601e4376f97957289b1fec210
matlabbe <matlabbe@gmail.com>
2024-01-04 09:04:02 -0800

    Rebase from 'rpm/humble/rtabmap_sync'

--- include/rtabmap_sync/rgb_sync.hpp
+++ include/rtabmap_sync/rgb_sync.hpp
@@ -39,6 +39,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <message_filters/subscriber.h>
 
 #include "rtabmap_msgs/msg/rgbd_image.hpp"
+#include "rtabmap_sync/SyncDiagnostic.h"
 
 namespace rtabmap_sync
 {
@@ -57,12 +58,8 @@ public:
 
 private:
 	double compressedRate_;
-	std::thread * warningThread_;
-	bool callbackCalled_;
 
 	rclcpp::Time lastCompressedPublished_;
-
-	std::string subscribedTopicsMsg_;
 
 	rclcpp::Publisher<rtabmap_msgs::msg::RGBDImage>::SharedPtr rgbdImagePub_;
 	rclcpp::Publisher<rtabmap_msgs::msg::RGBDImage>::SharedPtr rgbdImageCompressedPub_;
@@ -75,6 +72,8 @@ private:
 
 	typedef message_filters::sync_policies::ExactTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> MyExactSyncPolicy;
 	message_filters::Synchronizer<MyExactSyncPolicy> * exactSync_;
+
+	std::unique_ptr<SyncDiagnostic> syncDiagnostic_;
 };
 
 }
